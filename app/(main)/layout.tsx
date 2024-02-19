c96153773cc138b738b9ad533827df7ab30be961
ayush ranjan <asayushranjansinha@gmail.com>
2024-02-19 23:14:07 +0530

    changed nav import

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import AuthSessionProvider from "@/components/auth/auth-session-provider";
-import Navbar from "@/components/navbar";
+import Navbar from "../../components/navbar";
 
 
 const inter = Inter({ subsets: ["latin"] });
